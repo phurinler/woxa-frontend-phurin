25b5b2357196109683a130c9ebea06847c6f8180
phurin <le.phurin.work@gmail.com>
2025-05-08 03:42:29 +0700

    add read more button

--- src/components/card/CompanyCard.jsx
+++ src/components/card/CompanyCard.jsx
@@ -4,15 +4,18 @@ import StarRating from "../rating/StarRating";
 
 const CompanyCard = ({ company }) => {
   return (
-    <div className="flex flex-col gap-y-3 hover:bg-slate-200 border dark:hover:bg-slate-600 border-slate-300 hover:border-slate-200 dark:hover:border-slate-600 rounded-lg py-4 px-8 cursor-pointer">
+    <div className="group flex flex-col gap-y-3 hover:bg-slate-200 border dark:hover:bg-slate-600 border-slate-300 hover:border-slate-200 dark:hover:border-slate-600 rounded-lg py-4 px-8 cursor-pointer">
       {/* ลิงก์รูปจาก json ใช้งานไม่ได้แล้วจึงใช้เป็นรูปแบบ static */}
       <Image src="/waxo.png" width="80" height="80" alt="company" />
       <div className="flex gap-x-3">
         <Badge>{company.category}</Badge>
         <StarRating score={company.averageScore} />
       </div>
-      <div className="flex gap-x-3 items-end">
+      <div className="flex gap-x-3 justify-between items-end">
         <h4 className="text-md md:text-lg font-bold">{company.name}</h4>
+        <span className="text-sm md:text-md text-blue-500 group-hover:text-blue-400 group-hover:underline font-semibold">
+          Read More
+        </span>
       </div>
     </div>
   );
