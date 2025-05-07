221486bd7db62befd5c036efe4d431346d6f968a
phurin <le.phurin.work@gmail.com>
2025-05-08 04:35:24 +0700

    add lottie for empty page component

--- src/components/empty/EmptyPage.jsx
+++ src/components/empty/EmptyPage.jsx
@@ -1,6 +1,13 @@
+import dynamic from "next/dynamic";
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+import LottieAnimation from "../../assets/lottie.json";
+
 const EmptyPage = () => {
   return (
-    <section className=" max-w-7xl w-full h-full mx-auto my-auto flex flex-col gap-y-12 py-12 px-8 md:px-16">
+    <section className=" max-w-7xl w-full h-full mx-auto flex flex-col py-12 px-8 md:px-16">
+      <div className="flex justify-center max-h-[320px]">
+        <Lottie animationData={LottieAnimation} loop />
+      </div>
       <div className="flex flex-col justify-center items-center gap-y-6 text-center border-2 border-slate-400 rounded-4xl p-6 md:p-12 w-fit mx-auto">
         <p className="text-lg text-slate-400">WE'RE STILL</p>
         <p className="text-6xl font-bold text-blue-700">Cooking Our Website</p>
